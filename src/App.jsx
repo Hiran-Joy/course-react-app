db236140ce4dc7a73ac78ec04a05d0304955284f
Hiran-Joy <hiranjoy7@gmail.com>
2026-07-12 19:59:02 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-<<<<<<< HEAD
-<<<<<<< HEAD
 import InsertCourse from './components/InsertCourse'
-=======
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ViewCourses from './components/ViewCourses'
->>>>>>> viewCourses
-=======
-import InsertCourse from './components/InsertCourse'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import ViewCourses from './components/ViewCourse'
->>>>>>> navigationBar
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
-     <InsertCourse/>
-=======
-     <ViewCourses/>
->>>>>>> viewCourses
-=======
       <BrowserRouter>
         <Routes>
-
           <Route path='/' element={<InsertCourse/>} />
           <Route path='/view' element={<ViewCourses />} />
         </Routes>
       </BrowserRouter>
-
->>>>>>> navigationBar
     </>
   )
 }
